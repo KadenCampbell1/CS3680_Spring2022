8531c6d6f655aece80e63cbf226f73e7055b945b
Kaden(LenLap) <10836246@my.uvu.edu>
2022-01-19 23:54:10 -0700

    Finished project 1

--- P1_project/app/src/main/java/com/example/kc_project1/MainActivity.kt
+++ P1_project/app/src/main/java/com/example/kc_project1/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.kc_project1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var count = 1110100100
+        Log.i("CS3680", "${count}, but what does it mean?")
     }
 }
