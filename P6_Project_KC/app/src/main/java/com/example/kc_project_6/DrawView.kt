1ae124eae2d65815718e70d603235d1c22782042
Kaden(LenLap) <10836246@my.uvu.edu>
2022-04-08 17:37:13 -0600

    Finished project 6

--- P6_Project_KC/app/src/main/java/com/example/kc_project_6/DrawView.kt
+++ P6_Project_KC/app/src/main/java/com/example/kc_project_6/DrawView.kt
@@ -30,8 +30,10 @@ class DrawView(context: Context, attrs: AttributeSet? = null): View(context, att
     )
 
     var playerPosition = arrayListOf<Int>(2,2)
-    var nextX = 5F
-    var nextY = 5F
+    var gridRow = 0
+    var gridColumn = 0
+    var nextX = 85F
+    var nextY = 300F
     var rect: RectF
     val bwidth = 175
     val bheight = 175
@@ -87,23 +89,49 @@ class DrawView(context: Context, attrs: AttributeSet? = null): View(context, att
         if (event.action == MotionEvent.ACTION_UP) {
             Log.i("CS3680", "Touch event ${event.x}, ${event.y}")
             Log.i("CS3680", "$playerPosition")
-//            rect.left = event.x
-//            rect.top = event.y
-//            rect.right = event.x + bwidth
-//            rect.bottom = event.y + bheight
-//            invalidate()
+            if (event.y > 1000){
+                if (playerPosition[0] >= 13){
+                    return true
+                }
+                playerPosition[0] += 1
+                gridColumn += 1
+            }
+            if (event.y < 475){
+                if (playerPosition[0] <= 2){
+                    return true
+                }
+                playerPosition[0] -= 1
+                gridColumn -= 1
+            }
+            if (event.y > 475 && event.y < 1000 && event.x < 515){
+                if (playerPosition[1] <= 2){
+                    return true
+                }
+                playerPosition[1] -= 1
+                gridRow -= 1
+            }
+            if (event.y > 475 && event.y < 1000 && event.x > 515){
+                if (playerPosition[1] >= 13){
+                    return true
+                }
+                playerPosition[1] += 1
+                gridRow += 1
+            }
+            rect.left = nextX
+            rect.top = nextY
+            rect.right = nextX + bwidth
+            rect.bottom = nextY + bheight
+            invalidate()
             return true
         }
         return true
     }
 
     fun createGrid(canvas: Canvas){
-        var gridRow = 4
-        var gridColumn = 0
         var gridLetter = ""
 
-        for (column in gridColumn..gridRow){
-            for (row in gridColumn..gridRow){
+        for (column in gridColumn..gridColumn + 4){
+            for (row in gridRow..gridRow + 4){
                 gridLetter = mapGrid[column][row].toString()
                 if (gridLetter == "f"){
                     canvas.drawBitmap(forest_bitmap, null, rect, null)
